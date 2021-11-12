404b9ff7a0c30ff2bf2a33077fd86dd39931acea
lucasVictor <lucas.v0697@gmail.com>
2021-11-12 11:29:26 -0300

    Promotores bloqueados temporariamente

--- src/functions/celebre/fx.login.php
+++ src/functions/celebre/fx.login.php
@@ -8,7 +8,11 @@ function login($user_name = null, $password = null) {
   $user->set_usuario($user_name);
   $user->set_senha($password);
 
+  
   $login = $user->login();
+
+  if ($login[0]['user_level'] == '3')
+    return false;
 
   return $login;
 
