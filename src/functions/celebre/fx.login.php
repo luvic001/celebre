8aa0c430edfd4ddd9a7fa9892aa972645c7dc53f
lucasVictor <lucas.v0697@gmail.com>
2021-11-12 12:01:57 -0300

    Desbloqueio temporário dos promotoroes

--- src/functions/celebre/fx.login.php
+++ src/functions/celebre/fx.login.php
@@ -11,8 +11,8 @@ function login($user_name = null, $password = null) {
   
   $login = $user->login();
 
-  if ($login[0]['user_level'] == '3')
-    return false;
+  // if ($login[0]['user_level'] == '3')
+  //   return false;
 
   return $login;
 
